86806f66ae487a9b62514ab65d9dc944f2b9e20a
김주현 <j828h@naver.com>
2023-06-19 18:55:19 +0900

    feat: add http api, configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,7 @@ plugins {
 	kotlin("plugin.spring") version "1.8.21"
 	kotlin("plugin.jpa") version "1.8.21"
 	kotlin("plugin.allopen") version "1.8.0"
+	kotlin("kapt") version "1.8.0"
 }
 
 group = "com.practice"
@@ -27,14 +28,15 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("com.h2database:h2")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
 		exclude(module = "mockito-core")
 	}
 	testImplementation("org.junit.jupiter:junit-jupiter-api")
 	testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+	testImplementation("com.ninja-squad:springmockk:4.0.0")
 }
 
 tasks.withType<KotlinCompile> {
